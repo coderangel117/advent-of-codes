11ef04142358df02e7f2b9dfcd696c2ae9bbd6e5
gab <perinogabriel@gmail.com>
2024-12-03 22:30:12 +0100

    feat(day_1): resolve part 2  - Separate main function with 1 function per part

--- 2024/1/main.py
+++ 2024/1/main.py
@@ -17,7 +17,7 @@ def define_list():
     return left_list, right_list
 
 
-def main():
+def get_differences_from_lists(a, b):
     difference = 0
     a, b = define_list()
     a.sort()
@@ -25,6 +25,20 @@ def main():
     for i in range(len(a)):
         difference += abs(int(a[i]) - int(b[i]))
     print(difference)
+
+
+def get_similarities_from_lists(a, b):
+    similarity = 0
+    a, b = define_list()
+    for i in range(len(a)):
+        similarity += int(a[i]) * b.count(a[i])
+    print(similarity)
+
+
+def main():
+    a, b = define_list()
+    # get_differences_from_lists(a, b)
+    get_similarities_from_lists(a, b)
 
 
 if __name__ == "__main__":
