19283e30e3bf73558a4a92ab585c73d292e00e5d
gab <perinogabriel@gmail.com>
2024-12-03 21:34:46 +0100

    feat(day_1): resolve first part

---
+++ 2024/1/main.py
@@ -0,0 +1,38 @@
+def get_file_content(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
+
+
+def define_list():
+    file = get_file_content("input.txt").split()
+    left_list = []
+    right_list = []
+
+    for i in range(len(file)):
+        if i % 2 == 0:
+            left_list.append(file[i])
+        else:
+            right_list.append(file[i])
+
+    return left_list, right_list
+
+
+def sort_list():
+    left_list, right_list = define_list()
+    left_list.sort()
+    right_list.sort()
+    return left_list, right_list
+
+
+def main():
+    difference = 0
+    a, b = define_list()
+    a.sort()
+    b.sort()
+    for i in range(len(a)):
+        difference += abs(int(a[i]) - int(b[i]))
+    print(difference)
+
+
+if __name__ == "__main__":
+    main()
